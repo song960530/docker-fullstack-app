3473786750cc1b71bccc4f071e4240527a3c5115
송문준 <song960530@naver.com>
2022-01-22 18:44:54 +0900

    App.js 내용 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [lists, setLists] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    // 여기서 데이터베이스에 있는 값을 가져온다.
+    axios.get("/api/values").then(response => {
+      console.log("response",response.data);
+      setLists(reponse.data);
+    });
+  }, []);
+
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios.post("/api/value", {value: value}).then(response => {
+      if(response.data.success){
+        console.log("response",response);
+        setLists([...lists,response.data]);
+        setValue("");
+      }else {
+        alert("값을 DB에 넣는데 실패하였습니다.")
+      }
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+
+          {lists && lists.map((list,index) => (
+            <li key={index}>list.value</li>
+          ))};
+          <br/>
+
+          <form className="example" onSubmit={submitHandler}>
+            <input type="text" placeholder="입력해주세요" onChange={changeHandler} value={value}></input>
+            <button type="submit">확인</button>
+          </form>
+        </div>
       </header>
     </div>
   );
